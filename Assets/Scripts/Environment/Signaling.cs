6e77325c6ac2935a7f8f38246550e6da11687e8b
namespace-red <67328203+namespace-red@users.noreply.github.com>
2023-08-23 10:33:35 +0400

    Code review
    Did a normal coroutine check.

--- Assets/Scripts/Environment/Signaling.cs
+++ Assets/Scripts/Environment/Signaling.cs
@@ -10,7 +10,6 @@ public class Signaling : MonoBehaviour
     private Coroutine _activeCoroutine;
     private AudioSource _audioSource;
     private float _targetVolume;
-    private bool _isCoroutineRunning = false;
     
     private void Awake()
     {
@@ -20,27 +19,21 @@ public class Signaling : MonoBehaviour
 
     private void StartVolumeChangeCoroutine()
     {
-        if (_isCoroutineRunning)
+        if (_activeCoroutine != null)
         {
             StopCoroutine(_activeCoroutine);
         }
-        else
-        {
-            _activeCoroutine = StartCoroutine(ChangeVolume());
-        }
+        
+        _activeCoroutine = StartCoroutine(ChangeVolume());
     }
 
     private IEnumerator ChangeVolume()
     {
-        _isCoroutineRunning = true;
-        
         while (_audioSource.volume != _targetVolume)
         {
             _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolume, _volumeChangeRate * Time.deltaTime);
             yield return null;
         }
-        
-        _isCoroutineRunning = false;
     }
     
     public void PlaySignal()
