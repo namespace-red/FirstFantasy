c8ed624b6a62392765164cf2e021e28408502575
namespace-red <67328203+namespace-red@users.noreply.github.com>
2024-10-25 19:16:38 +0300

    Code review Signaling

--- Assets/Scripts/Environment/Signaler.cs
+++ Assets/Scripts/Environment/Signaler.cs
@@ -3,21 +3,40 @@ using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 
-public class Signaling : MonoBehaviour
+public class Signaler : MonoBehaviour
 {
     [SerializeField] private float _volumeChangeRate = 0.5f;
     
     private Coroutine _activeCoroutine;
     private AudioSource _audioSource;
     private float _targetVolume;
-    
+
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.volume = 0f;
     }
 
-    private void StartVolumeChangeCoroutine()
+    public void PlaySignal()
+    {
+        _audioSource.loop = true;
+        _audioSource.Play();
+        _targetVolume = 1f;
+        
+        StartVolumeChanging();
+    }
+
+    public void StopSignal()
+    {
+        _audioSource.loop = false;
+        _audioSource.Stop();
+        _audioSource.Play();
+        _targetVolume = 0f;
+        
+        StartVolumeChanging();
+    }
+
+    private void StartVolumeChanging()
     {
         if (_activeCoroutine != null)
         {
@@ -33,26 +52,10 @@ public class Signaling : MonoBehaviour
         {
             _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolume, 
                 _volumeChangeRate * Time.deltaTime);
+            
             yield return null;
         }
-    }
-    
-    public void PlaySignal()
-    {
-        _audioSource.loop = true;
-        _audioSource.Play();
-        _targetVolume = 1f;
-        
-        StartVolumeChangeCoroutine();
-    }
 
-    public void StopSignal()
-    {
-        _audioSource.loop = false;
-        _audioSource.Stop();
-        _audioSource.Play();
-        _targetVolume = 0f;
-        
-        StartVolumeChangeCoroutine();
+        _activeCoroutine = null;
     }
 }
